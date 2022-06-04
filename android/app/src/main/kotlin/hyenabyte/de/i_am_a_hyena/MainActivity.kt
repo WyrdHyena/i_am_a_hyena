f2b3a31958bbbdfc1ff61073c3c5feb6361dea22
WyrdHyena <89941859+WyrdHyena@users.noreply.github.com>
2022-06-04 17:16:17 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/hyenabyte/de/i_am_a_hyena/MainActivity.kt
@@ -0,0 +1,6 @@
+package hyenabyte.de.i_am_a_hyena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//The "main"-Function is the starting point for all Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        //Here comes the Text!
+        child: Text('I am a Hyena!'),
+      ),
+    ),
+  );
+}
